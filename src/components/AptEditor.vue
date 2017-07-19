ee8d34383149059b0243a78fc54f9c38ddb637a7
Ann Yanich <ann.yanich@gmail.com>
2017-07-19 21:36:55 +0200

    Use a Vue Watcher to react to user's APT plaintext input
    Automatically POST it.  (The interface is currently a simple mockup.)

--- src/components/AptEditor.vue
+++ src/components/AptEditor.vue
@@ -3,24 +3,29 @@
     <h2>Editor</h2>
     Here is the text in the editor:
     <div id="editor-content">
-      {{ content }}
+      {{ textInput }}
     </div>
     Here is the graph we have:
-    <div id="graph">
+    <div id="saved-graph">
       {{ graph }}
     </div>
+    Here is the graph we are previewing:
+    <div id="graph-preview">
+      {{ graphPreview }}
+    </div>
     <div>
-      <textarea id="text-entry" class="form-control" v-model="content"></textarea>
-      <button type="button" class="btn btn-primary pull-right" @click="renderGraph">Render Graph</button>
+      <textarea id="text-entry" class="form-control" v-model="textInput"></textarea>
+      <button type="button" class="btn btn-primary pull-right" v-on:click="saveGraph">Save Graph</button>
     </div>
   </div>
 </template>
 
 <script>
-//  import * as Vue from 'vue'
+  //  import * as Vue from 'vue'
 
   import * as axios from 'axios'
   import * as MockAdapter from 'axios-mock-adapter'
+
   let mock = new MockAdapter(axios)
 
   mock.onPost('/aptToGraph', {
@@ -34,30 +39,52 @@
     }
   })
 
+  mock.onPost('/aptToGraph').reply(200, {
+    graph: {
+      nodes: [],
+      edges: []
+    }
+  })
+
   export default {
     name: 'apt-editor',
     components: {},
     data () {
       return {
+        graphPreview: {
+          nodes: [],
+          edges: []
+        },
         graph: {
           nodes: [],
           edges: []
         },
-        content: '# Type APT into here.  \nClick "Send" to send it to ADAM and turn it into a graph.'
+        textInput: '# Type APT into here.  \nClick "Send" to send it to ADAM and turn it into a graph.'
       }
     },
     methods: {
       renderGraph: function () {
-        let vm = this
         axios.post('/aptToGraph', {
           params: {
-            aptSourceCode: '1 -> 2'
+            aptSourceCode: this.textInput
           }
         }).then(response => {
           console.log(response.data)
-//          TODO: Find out how to use the response in Vue
-          vm.$set(this.data, 'graph', response.data.graph)
+          this.graphPreview = response.data.graph
         })
+      },
+      saveGraph: function () {
+        this.graph = this.graphPreview
+        // TODO React to this event in a parent component, e.g. copy the graph into the drag-and-drop UI.
+        this.$emit('graphSaved')
+      }
+    },
+    watch: {
+      textInput: function (oldText, newText) {
+        console.log('textInput(' + oldText + ', ' + newText + ')')
+        if (oldText !== newText) {
+          this.renderGraph()
+        }
       }
     }
   }
