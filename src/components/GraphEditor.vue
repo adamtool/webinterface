a5f762557ceb028616e494ff00eea41ee01b0579
Ann Yanich <ann.yanich@gmail.com>
2017-08-30 01:11:12 +0200

    Get force layout to work (not yet interactive)

--- src/components/GraphEditor.vue
+++ src/components/GraphEditor.vue
@@ -10,6 +10,10 @@
         <div class="col-12">
           Graph passed in from our parent:
           <div>{{ parentGraph }}</div>
+          <!--Our nodes:-->
+          <!--<div>{{ nodes }}</div>-->
+          <!--Our links:-->
+          <!--<div>{{ links }}</div>-->
         </div>
       </div>
       <div class="row">
@@ -47,7 +51,37 @@
     components: {},
     data () {
       return {
-        exportedGraphJson: {}
+        exportedGraphJson: {},
+        nodes: [
+          {id: 'mammal', group: 0, label: 'Mammals', level: 1},
+          {id: 'dog', group: 0, label: 'Dogs', level: 2},
+          {id: 'cat', group: 0, label: 'Cats', level: 2},
+          {id: 'fox', group: 0, label: 'Foxes', level: 2},
+          {id: 'elk', group: 0, label: 'Elk', level: 2},
+          {id: 'insect', group: 1, label: 'Insects', level: 1},
+          {id: 'ant', group: 1, label: 'Ants', level: 2},
+          {id: 'bee', group: 1, label: 'Bees', level: 2},
+          {id: 'fish', group: 2, label: 'Fish', level: 1},
+          {id: 'carp', group: 2, label: 'Carp', level: 2},
+          {id: 'pike', group: 2, label: 'Pikes', level: 2}
+        ],
+        links: [
+          {target: 'mammal', source: 'dog', strength: 0.7},
+          {target: 'mammal', source: 'cat', strength: 0.7},
+          {target: 'mammal', source: 'fox', strength: 0.7},
+          {target: 'mammal', source: 'elk', strength: 0.7},
+          {target: 'insect', source: 'ant', strength: 0.7},
+          {target: 'insect', source: 'bee', strength: 0.7},
+          {target: 'fish', source: 'carp', strength: 0.7},
+          {target: 'fish', source: 'pike', strength: 0.7},
+
+          {target: 'cat', source: 'elk', strength: 0.1},
+          {target: 'carp', source: 'ant', strength: 0.1},
+          {target: 'elk', source: 'bee', strength: 0.1},
+          {target: 'dog', source: 'cat', strength: 0.1},
+          {target: 'fox', source: 'ant', strength: 0.1},
+          {target: 'pike', source: 'dog', strength: 0.1}
+        ]
       }
     },
     mounted: function () {
@@ -81,43 +115,13 @@
     },
     methods: {
       initializeD3: function () {
-        const nodes = [
-          {id: 'mammal', group: 0, label: 'Mammals', level: 1},
-          {id: 'dog', group: 0, label: 'Dogs', level: 2},
-          {id: 'cat', group: 0, label: 'Cats', level: 2},
-          {id: 'fox', group: 0, label: 'Foxes', level: 2},
-          {id: 'elk', group: 0, label: 'Elk', level: 2},
-          {id: 'insect', group: 1, label: 'Insects', level: 1},
-          {id: 'ant', group: 1, label: 'Ants', level: 2},
-          {id: 'bee', group: 1, label: 'Bees', level: 2},
-          {id: 'fish', group: 2, label: 'Fish', level: 1},
-          {id: 'carp', group: 2, label: 'Carp', level: 2},
-          {id: 'pike', group: 2, label: 'Pikes', level: 2}
-        ]
-
 //        const groups = {
 //          0: 'mammal',
 //          1: 'insect',
 //          2: 'fish'
 //        }
-
-        const links = [
-          {target: 'mammal', source: 'dog', strength: 0.7},
-          {target: 'mammal', source: 'cat', strength: 0.7},
-          {target: 'mammal', source: 'fox', strength: 0.7},
-          {target: 'mammal', source: 'elk', strength: 0.7},
-          {target: 'insect', source: 'ant', strength: 0.7},
-          {target: 'insect', source: 'bee', strength: 0.7},
-          {target: 'fish', source: 'carp', strength: 0.7},
-          {target: 'fish', source: 'pike', strength: 0.7},
-
-          {target: 'cat', source: 'elk', strength: 0.1},
-          {target: 'carp', source: 'ant', strength: 0.1},
-          {target: 'elk', source: 'bee', strength: 0.1},
-          {target: 'dog', source: 'cat', strength: 0.1},
-          {target: 'fox', source: 'ant', strength: 0.1},
-          {target: 'pike', source: 'dog', strength: 0.1}
-        ]
+        const nodes = this.nodes
+        const links = this.links
 
         const width = window.innerWidth
         const height = window.innerHeight
@@ -128,7 +132,7 @@
           .attr('height', height)
 
         const simulation = d3.forceSimulation()
-          .force('charge', d3.forceManyBody().strength(-20))
+          .force('charge', d3.forceManyBody().strength(-120))
           .force('center', d3.forceCenter(width / 2, height / 2))
 
         function getNodeColor (node) {
@@ -158,29 +162,25 @@
           textElements
             .attr('x', node => node.x)
             .attr('y', node => node.y)
+          linkElements
+            .attr('x1', link => link.source.x)
+            .attr('y1', link => link.source.y)
+            .attr('x2', link => link.target.x)
+            .attr('y2', link => link.target.y)
         })
         simulation.force('link', d3.forceLink()
           .id(link => link.id)
           .strength(link => link.strength))
 
+        simulation.force('link').links(links)
+
         const linkElements = svg.append('g')
           .selectAll('line')
           .data(links)
           .enter().append('line')
-          .attr('stroke-width', 1)
+          .attr('stroke-width', 5)
           .attr('stroke', '#E5E5E5')
 
-        linkElements
-          .attr('x1', link => {
-            console.log('link.source: (It should be an object with a property called "x"')
-            console.log(link.source)
-            return link.source.x
-          })
-          .attr('y1', link => link.source.y)
-          .attr('x2', link => link.target.x)
-          .attr('y2', link => link.target.y)
-
-        simulation.force('link').links(links).id(link => link.id)
         d3.selectAll('*').on('click', function (d) { console.log(d) })
       },
 
