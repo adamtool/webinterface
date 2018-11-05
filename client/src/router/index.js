0c519c7ae479f918ee955631b0b8599967bede47
Ann Yanich <ann.yanich@gmail.com>
2018-11-05 12:47:19 +0100

    You can switch between Model Checking and other GUI using the browser URL
    'http://localhost:4567/#/otherApproach'
    'http://localhost:4567/#/modelChecking'

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import AppModelChecking from '../AppModelChecking'
+import AppOtherApproach from '../AppOtherApproach'
 
 Vue.use(Router)
 
@@ -15,6 +16,11 @@ export default new Router({
       path: '/',
       name: 'Adam Web',
       component: AppModelChecking
+    },
+    {
+      path: '/otherApproach',
+      name: 'ADAM Web (not for model checking)', // TODO rename
+      component: AppOtherApproach
     }
   ]
 })
