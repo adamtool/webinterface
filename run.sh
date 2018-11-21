7c52822c4439c7f0e94fb9b941acff53dcb4345d
Ann Yanich <ann.yanich@posteo.de>
2018-11-21 14:53:07 +0100

    Update run script and ADAM.properties

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -jar server/target/web-frontend-1.0-SNAPSHOT-jar-with-dependencies.jar &
+java -jar server/target/web-frontend-1.0-SNAPSHOT-jar-with-dependencies.jar -DPROPERTY_FILE=./ADAM.properties &
 echo $! > ../adamweb.pid
