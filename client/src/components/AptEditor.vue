faa109002c3f8f34ce6bb5df60a0a22e1d48a8d6
Ann Yanich <ann.yanich@posteo.de>
2019-03-04 16:00:10 +0100

    Select the line of text where syntax error appears. (It's kind of annoying though)

--- client/src/components/AptEditor.vue
+++ client/src/components/AptEditor.vue
@@ -3,7 +3,11 @@
     <div style="text-align: center; flex: 0 0 58px; line-height: 58px; font-size: 18pt;">
       APT Editor
     </div>
-    <textarea class='apt-text-area' style="flex: 1 1 100%" v-model='aptInTextField' @input="emitAptChanged"/>
+    <textarea class='apt-text-area'
+              style="flex: 1 1 100%"
+              v-model='aptInTextField'
+              @input="emitAptChanged"
+              ref="theTextArea"/>
     <div style="color: red;">{{ aptParseError }}
     </div>
   </div>
@@ -48,18 +52,67 @@
     methods: {
       emitAptChanged: function () {
         this.$emit('input', this.aptInTextField)
+      },
+      // Select a specific line of text in the text area
+      selectTextAreaLine: function (tarea, lineNum) {
+        lineNum-- // array starts at 0
+        const lines = tarea.value.split('\n')
+
+        // calculate start/end
+        let startPos = 0
+        let endPos = tarea.value.length
+        for (let x = 0; x < lines.length; x++) {
+          if (x === lineNum) {
+            break
+          }
+          startPos += (lines[x].length + 1)
+        }
+
+        endPos = lines[lineNum].length + startPos
+
+        // do selection
+        // Chrome / Firefox
+
+        if (typeof (tarea.selectionStart) !== 'undefined') {
+          tarea.focus()
+          tarea.selectionStart = startPos
+          tarea.selectionEnd = endPos
+          return true
+        }
+
+        // IE
+        if (document.selection && document.selection.createRange) {
+          tarea.focus()
+          tarea.select()
+          const range = document.selection.createRange()
+          range.collapse(true)
+          range.moveEnd('character', endPos)
+          range.moveStart('character', startPos)
+          range.select()
+          return true
+        }
+        return false
       }
     },
     watch: {
       aptFromAdamParser: function () {
         this.aptInTextField = this.aptFromAdamParser
+      },
+      // When there's a parse error, highlight the corresponding line of text in the APT editor
+      aptParseStatus: function (status) {
+        if (status === 'error' && this.isParseErrorHighlightingInfoPresent) {
+          this.selectTextAreaLine(this.$refs.theTextArea, this.aptParseErrorLineNumber)
+        }
       }
     },
     computed: {
-      lineAndColumnText: function () {
+      isParseErrorHighlightingInfoPresent: function () {
         const isLineNumberValid = this.aptParseErrorLineNumber !== -1 && this.aptParseErrorLineNumber !== undefined
         const isColumnNumberValid = this.aptParseErrorColumnNumber !== -1 && this.aptParseErrorColumnNumber !== undefined
-        if (!isLineNumberValid && !isColumnNumberValid) {
+        return isLineNumberValid && isColumnNumberValid
+      },
+      lineAndColumnText: function () {
+        if (!this.isParseErrorHighlightingInfoPresent) {
           return ''
         }
         return `(line ${this.aptParseErrorLineNumber}, column ${this.aptParseErrorColumnNumber})`
