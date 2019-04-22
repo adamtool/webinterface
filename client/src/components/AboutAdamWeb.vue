5bdc5a1039b3e3a15c730316e92f1e6a0c268d32
Ann Yanich <ann.yanich@posteo.de>
2019-04-22 22:17:03 +0200

    Revise "About" screen.  Hide extra info behind an accordion

--- client/src/components/AboutAdamWeb.vue
+++ client/src/components/AboutAdamWeb.vue
@@ -1,16 +1,6 @@
 <template>
   <div style="">
     <p>
-      This version of ADAM Web was built at {{ formattedBuildDate }}
-      using git revision {{ sourceCodeRevision }}.
-    </p>
-
-    <p>
-      ADAM Web was developed by Ann Yanich under the supervision of Manuel Gieseking in the
-      department Entwicklung korrekter Systeme [Correct System Design] at the Universität Oldenburg.
-    </p>
-
-    <p>
       ADAM Web is a frontend for the library
       <a href="https://uol.de/csd/adam/" target="_blank">
         ADAM (Analyzer of Distributed Asynchronous Models).
@@ -18,25 +8,56 @@
     </p>
 
     <p>
-      ADAM Web uses open source components. You can find the source code of their open source
-      projects along with license information below.
+      It was developed by Ann Yanich under the supervision of Manuel Gieseking in the
+      Correct System Design group at the Universität Oldenburg.
     </p>
 
-    <ul>
-      <li v-for="(library, libraryName) in creditsJson">
-        {{ libraryName }}
-        <ul>
-          <li>License: <a :href="library.licenseUrl">{{ library.licenses }}</a></li>
-          <li><a :href="library.repository">Repository</a></li>
-        </ul>
-      </li>
-    </ul>
+    <v-expansion-panel>
+      <v-expansion-panel-content>
+        <template v-slot:header>
+          <div>Version</div>
+        </template>
+        <v-card>
+          <v-card-text>
+            <p>
+              Build date: {{ formattedBuildDate }}
+            </p>
+            <p>
+              Git revision: {{ sourceCodeRevision }}.
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-expansion-panel-content>
+      <v-expansion-panel-content>
+        <template v-slot:header>
+          <div>License information</div>
+        </template>
+        <v-card>
+          <v-card-text>
+            <p>
+              ADAM Web uses open source components. You can find the source code of their open
+              source
+              projects along with license information below.
+            </p>
+            <ul>
+              <li v-for="(library, libraryName) in creditsJson">
+                {{ libraryName }}
+                <ul>
+                  <li>License: <a :href="library.licenseUrl">{{ library.licenses }}</a></li>
+                  <li><a :href="library.repository">Repository</a></li>
+                </ul>
+              </li>
+            </ul>
+          </v-card-text>
+        </v-card>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
   </div>
 </template>
 
 <script>
   import creditsJson from '../assets/licenses-json.json'
-  import { format } from 'date-fns'
+  import {format} from 'date-fns'
 
   export default {
     name: 'AboutAdamWeb',
@@ -44,10 +65,10 @@
       creditsJson: () => creditsJson,
       buildDate: () => Date.parse(____ADAM_WEB_BUILD_DATE____),
       formattedBuildDate: function () {
-        return format(this.buildDate, 'HH:mm:ss on MMMM Do YYYY ')
+        return format(this.buildDate, 'HH:mm:ss, MMMM Do, YYYY ')
       },
       sourceCodeRevision: () => ____ADAM_WEB_BUILD_SHA____
-    },
+    }
   }
 </script>
 
