adamtool/webinterface

commit e3e1a90995d07e7d9142667265bfff7ed5e6b95b

Throw CouldNotFindSuitableConditionException when condition annotation is bad
The previous behavior was to just show the condition as an empty string.
This way, when you type a bad condition name into the APT editor, you
get an APT parse error right away.

server/src/main/java/uniolunisaar/adamwebfrontend/App.java
@@ -147,11 +147,14 @@ public class App {
 
         exception(Exception.class, (exception, request, response) -> {
             exception.printStackTrace();
-            String exceptionName = exception.getClass().getSimpleName();
-            String exceptionAsString = exceptionName + ": " + exception.getMessage();
+            String exceptionAsString = exceptionToString(exception);
             String responseBody = errorResponse(exceptionAsString);
             response.body(responseBody);
         });
+    }
+    private static String exceptionToString(Exception exception) {
+        String exceptionName = exception.getClass().getSimpleName();
+        return exceptionName + ": " + exception.getMessage();
     }
 
     /**
@@ -248,7 +251,7 @@ public class App {
         return responseJson.toString();
     }
 
-    private Object handleParseApt(Request req, Response res) {
+    private Object handleParseApt(Request req, Response res) throws CouldNotFindSuitableConditionException {
         JsonElement body = parser.parse(req.body());
         System.out.println("body: " + body.toString());
         String apt = body.getAsJsonObject().get("params").getAsJsonObject().get("apt").getAsString();
@@ -309,11 +312,15 @@ public class App {
 
     private Calculation<BDDGraphExplorer> calculateGraphGameBDD(PetriGame petriGame,
                                                                 JsonObject params) {
-        Optional<Condition.Objective> objective = PetriNetD3.getObjectiveOfPetriNet(petriGame);
-        if (!objective.isPresent()) {
-            throw new IllegalArgumentException(
-                    "No winning condition is present for the given Petri Game.");
+        // If there is an invalid condition annotation, we should throw an error right away instead
+        // of waiting until the calculation gets started (which might take a while if there is a
+        // queue).
+        try {
+            PetriNetD3.getObjectiveOfPetriNet(petriGame);
+        } catch (CouldNotFindSuitableConditionException e) {
+            throw new IllegalArgumentException(exceptionToString(e));
         }
+
         boolean shouldSolveStepwise = params.get("incremental").getAsBoolean();
         return new Calculation<>(() -> {
             if (shouldSolveStepwise) {
@@ -346,7 +353,7 @@ public class App {
      */
     private <T> Route handleQueueCalculation(CalculationFactory<T> calculationFactory,
                                              CalculationType calculationType,
-                                             Function<T, JsonElement> serializerFunction) {
+                                             SerializerFunction<T> serializerFunction) {
         return (req, res) -> {
             // Read request parameters.  Get the Petri Game that should be operated upon and the
             // UserContext of the client making the request.
@@ -408,11 +415,12 @@ public class App {
      * @param <T>              The result type of the calculation
      * @return A JSON response with the result, if it is ready within five seconds.
      * Otherwise, just a message that the calculation is still being calculated.
+     * @throws Exception if the serializer function throws an exception.
      */
     private static <T> Object tryToGetResultWithinFiveSeconds(Calculation<T> calculation,
-                                                              Function<T, JsonElement> resultSerializer,
+                                                              SerializerFunction<T> resultSerializer,
                                                               String canonicalApt,
-                                                              PetriGame petriGame) {
+                                                              PetriGame petriGame) throws Exception {
         try {
             T result = calculation.getResult(5, TimeUnit.SECONDS);
             JsonObject responseJson = new JsonObject();
@@ -447,7 +455,7 @@ public class App {
      * return the result of the job, if one has been queued and has completed successfully.
      */
     private <T> Route handleGetCalculationResult(CalculationType calculationType,
-                                                 Function<T, JsonElement> serializerFunction) {
+                                                 SerializerFunction<T> serializerFunction) {
         return (req, res) -> {
             JsonElement body = parser.parse(req.body());
             System.out.println("body: " + body.toString());
@@ -593,7 +601,7 @@ public class App {
         return responseJson.toString();
     }
 
-    private Object handleInsertPlace(Request req, Response res, PetriGame petriGame) {
+    private Object handleInsertPlace(Request req, Response res, PetriGame petriGame) throws CouldNotFindSuitableConditionException {
         JsonObject body = parser.parse(req.body()).getAsJsonObject();
         double x = body.get("x").getAsDouble();
         double y = body.get("y").getAsDouble();
@@ -625,7 +633,7 @@ public class App {
         return successResponse(petriGameClient);
     }
 
-    private Object handleDeleteNode(Request req, Response res, PetriGame petriGame) {
+    private Object handleDeleteNode(Request req, Response res, PetriGame petriGame) throws CouldNotFindSuitableConditionException {
         JsonObject body = parser.parse(req.body()).getAsJsonObject();
         String nodeId = body.get("nodeId").getAsString();
 
@@ -635,7 +643,7 @@ public class App {
         return successResponse(petriGameClient);
     }
 
-    private Object handleRenameNode(Request req, Response res, PetriGame petriGame) {
+    private Object handleRenameNode(Request req, Response res, PetriGame petriGame) throws CouldNotFindSuitableConditionException {
         JsonObject body = parser.parse(req.body()).getAsJsonObject();
         String nodeIdOld = body.get("nodeIdOld").getAsString();
         String nodeIdNew = body.get("nodeIdNew").getAsString();
@@ -647,7 +655,7 @@ public class App {
         return successResponse(petriGameClient);
     }
 
-    private Object handleToggleEnvironmentPlace(Request req, Response res, PetriGame petriGame) {
+    private Object handleToggleEnvironmentPlace(Request req, Response res, PetriGame petriGame) throws CouldNotFindSuitableConditionException {
         JsonObject body = parser.parse(req.body()).getAsJsonObject();
         String nodeId = body.get("nodeId").getAsString();
 
@@ -663,7 +671,7 @@ public class App {
         return successResponse(petriGameClient);
     }
 
-    private Object handleToggleIsInitialTokenFlow(Request req, Response res, PetriGame petriGame) {
+    private Object handleToggleIsInitialTokenFlow(Request req, Response res, PetriGame petriGame) throws CouldNotFindSuitableConditionException {
         JsonObject body = parser.parse(req.body()).getAsJsonObject();
         String nodeId = body.get("nodeId").getAsString();
 
@@ -679,7 +687,7 @@ public class App {
         return successResponse(petriGameClient);
     }
 
-    private Object handleSetInitialToken(Request req, Response res, PetriGame petriGame) {
+    private Object handleSetInitialToken(Request req, Response res, PetriGame petriGame) throws CouldNotFindSuitableConditionException {
         JsonObject body = parser.parse(req.body()).getAsJsonObject();
         String nodeId = body.get("nodeId").getAsString();
         int tokens = body.get("tokens").getAsInt();
@@ -691,7 +699,7 @@ public class App {
         return successResponse(petriGameClient);
     }
 
-    private Object handleSetWinningCondition(Request req, Response res, PetriGame petriGame) {
+    private Object handleSetWinningCondition(Request req, Response res, PetriGame petriGame) throws CouldNotFindSuitableConditionException {
         JsonObject body = parser.parse(req.body()).getAsJsonObject();
         String winningCondition = body.get("winningCondition").getAsString();
 
@@ -702,7 +710,7 @@ public class App {
         return successResponse(petriGameClient);
     }
 
-    private Object handleCreateFlow(Request req, Response res, PetriGame petriGame) {
+    private Object handleCreateFlow(Request req, Response res, PetriGame petriGame) throws CouldNotFindSuitableConditionException {
         JsonObject body = parser.parse(req.body()).getAsJsonObject();
         String source = body.get("source").getAsString();
         String destination = body.get("destination").getAsString();
@@ -713,7 +721,7 @@ public class App {
         return successResponse(petriGameClient);
     }
 
-    private Object handleDeleteFlow(Request req, Response res, PetriGame petriGame) {
+    private Object handleDeleteFlow(Request req, Response res, PetriGame petriGame) throws CouldNotFindSuitableConditionException {
         JsonObject body = parser.parse(req.body()).getAsJsonObject();
         String source = body.get("sourceId").getAsString();
         String target = body.get("targetId").getAsString();
@@ -724,7 +732,7 @@ public class App {
         return successResponse(petriGameClient);
     }
 
-    private Object handleCreateTokenFlow(Request req, Response res, PetriGame petriGame) {
+    private Object handleCreateTokenFlow(Request req, Response res, PetriGame petriGame) throws CouldNotFindSuitableConditionException {
         JsonObject body = parser.parse(req.body()).getAsJsonObject();
         Optional<String> sourceId = body.has("source") ?
                 Optional.of(body.get("source").getAsString()) :
@@ -778,7 +786,7 @@ public class App {
         }
     }
 
-    private Object handleGetModelCheckingNet(Request req, Response res, PetriGame petriGame) throws NotSupportedGameException, InterruptedException, ParseException, IOException, ExternalToolException, NotConvertableException, ProcessNotStartedException {
+    private Object handleGetModelCheckingNet(Request req, Response res, PetriGame petriGame) throws NotSupportedGameException, InterruptedException, ParseException, IOException, ExternalToolException, NotConvertableException, ProcessNotStartedException, CouldNotFindSuitableConditionException {
         JsonObject body = parser.parse(req.body()).getAsJsonObject();
         String formula = body.get("formula").getAsString();
 
@@ -796,7 +804,7 @@ public class App {
         return successResponse(PetriNetD3.of(new PetriGame(modelCheckingNet)));
     }
 
-    private Object handleFireTransition(Request req, Response res, PetriGame petriGame) {
+    private Object handleFireTransition(Request req, Response res, PetriGame petriGame) throws CouldNotFindSuitableConditionException {
         JsonObject body = parser.parse(req.body()).getAsJsonObject();
         String transitionId = body.get("transitionId").getAsString();
 

server/src/main/java/uniolunisaar/adamwebfrontend/PetriNetD3.java
@@ -9,10 +9,8 @@ import uniolunisaar.adam.ds.objectives.Condition;
 import uniolunisaar.adam.ds.petrigame.PetriGame;
 import uniolunisaar.adam.ds.petrinetwithtransits.PetriNetWithTransits;
 import uniolunisaar.adam.ds.petrinetwithtransits.Transit;
-import uniolunisaar.adam.exceptions.pg.NotSupportedGameException;
 import uniolunisaar.adam.exceptions.pnwt.CouldNotFindSuitableConditionException;
 import uniolunisaar.adam.tools.Tools;
-import uniolunisaar.adam.util.AdamExtensions;
 import uniolunisaar.adam.util.PNWTTools;
 
 import java.util.*;
@@ -48,7 +46,7 @@ public class PetriNetD3 {
      * <p>
      * See https://github.com/d3/d3-force
      */
-    public static JsonElement ofPetriGameWithXYCoordinates(PetriGame net, Set<Node> shouldSendPositions) {
+    public static JsonElement ofPetriGameWithXYCoordinates(PetriGame net, Set<Node> shouldSendPositions) throws CouldNotFindSuitableConditionException {
         List<PetriNetLink> links = new ArrayList<>();
         List<PetriNetNode> nodes = new ArrayList<>();
 
@@ -90,41 +88,25 @@ public class PetriNetD3 {
                         Node::getId, positionOfNode
                 ));
 
-        Optional<Condition.Objective> objectiveOfPetriNet = getObjectiveOfPetriNet(net);
-        if (objectiveOfPetriNet.isPresent()) {
-            Condition.Objective objective = objectiveOfPetriNet.get();
-            boolean canConvertToLtl = objective.equals(Condition.Objective.A_BUCHI) ||
-                    objective.equals(Condition.Objective.A_REACHABILITY) ||
-                    objective.equals(Condition.Objective.A_SAFETY);
-            String ltlFormula = canConvertToLtl ? AdamModelChecker.toFlowLTLFormula(net, objective) : "";
-            PetriNetD3 petriNetD3 = new PetriNetD3(links, nodes, nodePositions, objective.toString(),
-                    ltlFormula);
-            return new Gson().toJsonTree(petriNetD3);
-        } else {
-            PetriNetD3 petriNetD3 = new PetriNetD3(links, nodes, nodePositions, "", "");
-            return new Gson().toJsonTree(petriNetD3);
-        }
+        Condition.Objective objective = getObjectiveOfPetriNet(net);
+        boolean canConvertToLtl = objective.equals(Condition.Objective.A_BUCHI) ||
+                objective.equals(Condition.Objective.A_REACHABILITY) ||
+                objective.equals(Condition.Objective.A_SAFETY);
+        String ltlFormula = canConvertToLtl ? AdamModelChecker.toFlowLTLFormula(net, objective) : "";
+        PetriNetD3 petriNetD3 = new PetriNetD3(links, nodes, nodePositions, objective.toString(),
+                ltlFormula);
+        return new Gson().toJsonTree(petriNetD3);
     }
 
-    public static Optional<Condition.Objective> getObjectiveOfPetriNet(PetriNetWithTransits net) {
-        Optional<String> winningCondition = getWinningConditionOfPetriNet(net);
-        return winningCondition.map(Condition.Objective::valueOf);
+    public static Condition.Objective getObjectiveOfPetriNet(PetriNetWithTransits net) throws CouldNotFindSuitableConditionException {
+        String conditionString = Adam.getCondition(net).toString();
+        return Condition.Objective.valueOf(conditionString);
     }
-
-    public static Optional<String> getWinningConditionOfPetriNet(PetriNetWithTransits net) {
-        try {
-            return Optional.of(Adam.getCondition(net).toString());
-        } catch (CouldNotFindSuitableConditionException e) {
-            // TODO Instead of returning Optional, re-throw this exception
-            return Optional.empty();
-        }
-    }
-
 
     /**
      * @return a JSON representation of a Petri Game. Does not include any X/Y coordinate annotations.
      */
-    public static JsonElement of(PetriGame game) {
+    public static JsonElement of(PetriGame game) throws CouldNotFindSuitableConditionException {
         return ofPetriGameWithXYCoordinates(game, new HashSet<>());
     }
 

server/src/main/java/uniolunisaar/adamwebfrontend/BDDGraphExplorerStepwise.java
@@ -7,6 +7,7 @@ import uniolunisaar.adam.ds.graph.Flow;
 import uniolunisaar.adam.ds.objectives.Condition;
 import uniolunisaar.adam.ds.petrigame.PetriGame;
 import uniolunisaar.adam.exceptions.pg.SolvingException;
+import uniolunisaar.adam.exceptions.pnwt.CouldNotFindSuitableConditionException;
 import uniolunisaar.adam.symbolic.bddapproach.graph.BDDGraph;
 import uniolunisaar.adam.symbolic.bddapproach.graph.BDDState;
 import uniolunisaar.adam.symbolic.bddapproach.solver.BDDSolver;
@@ -26,10 +27,10 @@ public class BDDGraphExplorerStepwise implements BDDGraphExplorer {
     // States that we called getSuccessors() on already
     private final Set<BDDState> expandedStates;
 
-    public BDDGraphExplorerStepwise(PetriGame game) throws SolvingException {
+    public BDDGraphExplorerStepwise(PetriGame game) throws SolvingException, CouldNotFindSuitableConditionException {
         solver = AdamSynthesizer.getBDDSolver(
                 game,
-                PetriNetD3.getObjectiveOfPetriNet(game).get(),
+                PetriNetD3.getObjectiveOfPetriNet(game),
                 new BDDSolverOptions());
         bddGraph = new BDDGraph("My Graph");
         solver.initialize();

server/src/main/java/uniolunisaar/adamwebfrontend/SerializerFunction.java
@@ -1,0 +1,11 @@
+package uniolunisaar.adamwebfrontend;
+
+import com.google.gson.JsonElement;
+
+/**
+ * This interface represents a function that we can call to serialize an object of a given
+ * class into a JSON representation that our browser client can work with.
+ */
+public interface SerializerFunction<T> {
+    JsonElement apply(T t) throws Exception;
+}

commit 3d3806b94be1741acecf22d3342510d3d93f822d

Bug fix: The nets representing winning strategies could not be loaded in the client
I think the bug was introduced in commit e3e1a90995d07e7d9142667265bfff7ed5e6b95b

server/src/main/java/uniolunisaar/adamwebfrontend/App.java
@@ -84,7 +84,7 @@ public class App {
         post("/calculateStrategyBDD", handleQueueJob(
                 this::calculateStrategyBDD,
                 JobType.WINNING_STRATEGY,
-                PetriNetD3::of
+                PetriNetD3::ofNetWithoutObjective
         ));
 
         post("/calculateGraphStrategyBDD", handleQueueJob(
@@ -106,7 +106,7 @@ public class App {
 
         post("/getWinningStrategy", handleGetJobResult(
                 JobType.WINNING_STRATEGY,
-                PetriNetD3::of
+                PetriNetD3::ofNetWithoutObjective
         ));
 
         post("/getGraphStrategyBDD", handleGetJobResult(
@@ -293,7 +293,8 @@ public class App {
         petriGamesReadFromApt.put(petriGameUUID, petriGame);
         System.out.println("Generated petri game with ID " + petriGameUUID);
 
-        JsonElement petriNetD3Json = PetriNetD3.ofPetriGameWithXYCoordinates(petriGame, petriGame.getNodes());
+        JsonElement petriNetD3Json =
+                PetriNetD3.ofPetriGameWithXYCoordinates(petriGame, petriGame.getNodes(), true);
         JsonElement petriGameClient = PetriGameD3.of(petriNetD3Json, petriGameUUID);
 
         JsonObject responseJson = new JsonObject();
@@ -360,8 +361,8 @@ public class App {
      * and a browser/user-session UUID.
      * We retrieve the corresponding Petri Game and queue up the requested job.
      *
-     * @param jobFactory A function that creates the Job that should be run
-     * @param jobType    The result type of the job, used to save it in a
+     * @param jobFactory         A function that creates the Job that should be run
+     * @param jobType            The result type of the job, used to save it in a
      *                           corresponding Map
      * @param serializerFunction A function that will serialize the result of the job so it
      *                           can be sent to the client.
@@ -424,7 +425,7 @@ public class App {
      * to the job queue.
      * This method is meant to handle that type of response.
      *
-     * @param job      the job that has been queued just now
+     * @param job              the job that has been queued just now
      * @param resultSerializer A function to serialize the result of the job into JSON
      *                         for the client to consume
      * @param canonicalApt     The 'canonical apt' of the Petri Game that is being analyzed
@@ -645,7 +646,7 @@ public class App {
         petriGame.setYCoord(node, y);
 
         JsonElement petriGameClient = PetriNetD3.ofPetriGameWithXYCoordinates(
-                petriGame, new HashSet<>(Collections.singletonList(node)));
+                petriGame, new HashSet<>(Collections.singletonList(node)), true);
 
         return successResponse(petriGameClient);
     }

server/src/main/java/uniolunisaar/adamwebfrontend/PetriNetD3.java
@@ -42,11 +42,16 @@ public class PetriNetD3 {
      *
      * @param net                 - A PetriNet
      * @param shouldSendPositions - We will send x/y coordinates for these nodes to the client.
+       @param shouldIncludeObjective - For Petri Games, we want to include the objective/winning
+       condition, but for other Petri Nets (e.g. those which represent winning strategies), the
+       annotation may not be present, and we shouldn't try to send it to the client.
      * @return A JSON object containing the relevant information from the PetriNet
      * <p>
      * See https://github.com/d3/d3-force
      */
-    public static JsonElement ofPetriGameWithXYCoordinates(PetriGame net, Set<Node> shouldSendPositions) throws CouldNotFindSuitableConditionException {
+    public static JsonElement ofPetriGameWithXYCoordinates(PetriGame net,
+                                                           Set<Node> shouldSendPositions,
+                                                           boolean shouldIncludeObjective) throws CouldNotFindSuitableConditionException {
         List<PetriNetLink> links = new ArrayList<>();
         List<PetriNetNode> nodes = new ArrayList<>();
 
@@ -88,13 +93,22 @@ public class PetriNetD3 {
                         Node::getId, positionOfNode
                 ));
 
-        Condition.Objective objective = getObjectiveOfPetriNet(net);
-        boolean canConvertToLtl = objective.equals(Condition.Objective.A_BUCHI) ||
-                objective.equals(Condition.Objective.A_REACHABILITY) ||
-                objective.equals(Condition.Objective.A_SAFETY);
-        String ltlFormula = canConvertToLtl ? AdamModelChecker.toFlowLTLFormula(net, objective) : "";
-        PetriNetD3 petriNetD3 = new PetriNetD3(links, nodes, nodePositions, objective.toString(),
-                ltlFormula);
+        String objectiveString;
+        String ltlFormulaString;
+        if (shouldIncludeObjective) {
+            Condition.Objective objective = getObjectiveOfPetriNet(net);
+            boolean canConvertToLtl = objective.equals(Condition.Objective.A_BUCHI) ||
+                    objective.equals(Condition.Objective.A_REACHABILITY) ||
+                    objective.equals(Condition.Objective.A_SAFETY);
+            ltlFormulaString = canConvertToLtl ? AdamModelChecker.toFlowLTLFormula(net, objective) :
+                    "";
+            objectiveString = objective.toString();
+        } else {
+            objectiveString = "";
+            ltlFormulaString = "";
+        }
+        PetriNetD3 petriNetD3 = new PetriNetD3(links, nodes, nodePositions, objectiveString,
+                ltlFormulaString);
         return new Gson().toJsonTree(petriNetD3);
     }
 
@@ -107,8 +121,14 @@ public class PetriNetD3 {
      * @return a JSON representation of a Petri Game. Does not include any X/Y coordinate annotations.
      */
     public static JsonElement of(PetriGame game) throws CouldNotFindSuitableConditionException {
-        return ofPetriGameWithXYCoordinates(game, new HashSet<>());
+        return ofPetriGameWithXYCoordinates(game, new HashSet<>(), true);
     }
+
+    public static JsonElement ofNetWithoutObjective(PetriGame game) throws CouldNotFindSuitableConditionException {
+        return ofPetriGameWithXYCoordinates(game, new HashSet<>(), false);
+    }
+
+
 
     static class PetriNetLink extends GraphLink {
         private final String tokenFlow; // Null if there is no token flow given