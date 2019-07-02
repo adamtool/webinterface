0c8aa7db924b05409aef425490a09c1230e92356
Ann Yanich <ann.yanich@posteo.de>
2019-07-02 19:40:16 +0200

    chmod u+x build.sh.  Output logs to a txt file in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -jar server/target/web-frontend-1.0-SNAPSHOT-jar-with-dependencies.jar -DPROPERTY_FILE=./ADAM.properties &
+java -jar server/target/web-frontend-1.0-SNAPSHOT-jar-with-dependencies.jar -DPROPERTY_FILE=./ADAM.properties 2>&1 | tee adamweb-log-`date +%Y-%m-%d-%Hh%Mm%Ss`.txt &
 echo $! > ../adamweb.pid
