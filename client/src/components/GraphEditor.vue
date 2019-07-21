657779afd115c9f55624aa6dae398731e681078b
Ann Yanich <ann.yanich@posteo.de>
2019-07-21 19:15:49 +0200

    Center tool picker on left side of graph editor

--- client/src/components/GraphEditor.vue
+++ client/src/components/GraphEditor.vue
@@ -54,7 +54,8 @@
       </v-container>
     </div>
     <ToolPicker
-      style="position: absolute; z-index: 5; width: auto;"
+      style="position: absolute; top: 50%; transform: translate(0, -50%); z-index: 5; width:
+      auto;"
       :selectedTool="this.selectedTool"
       @onPickTool="tool => this.selectedTool = tool"
       :tools="this.toolPickerItems"/>
