2991213a6c87f4953b901eb75d410c7504c9ba31
Ann Yanich <ann.yanich@posteo.de>
2019-10-24 21:23:02 +0200

    Add File>New Petri game / New Petri net with transits

--- client/src/App.vue
+++ client/src/App.vue
@@ -81,6 +81,9 @@
       style="z-index: 999; display: flex; flex-direction: row; justify-content: space-between;">
       <hsc-menu-bar :style="menuBarStyle" ref="menubar">
         <hsc-menu-bar-item label="File">
+          <hsc-menu-item
+            :label="useModelChecking ? 'New Petri net with transits' : 'New Petri game'"
+            @click.native="onAptExampleSelected(aptExampleEmpty)"/>
           <!--Have to use click.native so that the popup blocker isn't triggered-->
           <hsc-menu-item label="Load APT from file" @click.native="loadAptFromFile"/>
           <v-dialog
@@ -542,6 +545,7 @@
 
   import aptExampleLtl from './somewhatSmallExampleLtl.apt'
   import aptExampleDistributedSynthesis from './somewhatSmallExampleNotLtl.apt'
+  import aptExampleEmpty from './aptExampleEmpty.apt'
   import HscMenuBarDirectory from './components/hsc-menu-bar-directory'
 
   import makeWebSocket from './logWebSocket'
@@ -727,6 +731,9 @@
       }
     },
     computed: {
+      aptExampleEmpty: function () {
+        return aptExampleEmpty
+      },
       tabsRightSide: function () {
         return this.visibleJobsRightSide.map((jobKey) => jobKeyToTab(this.jobListings, jobKey))
 
