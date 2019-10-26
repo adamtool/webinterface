6e68c2bc2ad391f2ed493aa6ee18d0e42486344b
Ann Yanich <ann.yanich@posteo.de>
2019-10-26 16:04:03 +0200

    Apply a key to the <pre> elements in LogViewer to improve rendering performance

--- client/src/components/LogViewer.vue
+++ client/src/components/LogViewer.vue
@@ -2,52 +2,44 @@
   <div class="logViewer" style="display: flex; flex-direction: column;">
     <div style="flex: 1 0 50px; display: flex; justify-content: center; align-items: center;">
       <v-radio-group v-model="logLevel" row hide-details style="padding-top: 0" height="30px">
-          <v-radio label="Verbose" :value="1"/>
-          <v-radio label="Normal" :value="2"/>
-          <v-radio label="Warning" :value="3"/>
-          <v-radio label="Error" :value="4"/>
-          <v-checkbox label="Show client log" v-model="showClientMessages"/>
-          <v-checkbox label="Show server log" v-model="showServerMessages"/>
+        <v-radio label="Verbose" :value="1"/>
+        <v-radio label="Normal" :value="2"/>
+        <v-radio label="Warning" :value="3"/>
+        <v-radio label="Error" :value="4"/>
+        <v-checkbox label="Show client log" v-model="showClientMessages"/>
+        <v-checkbox label="Show server log" v-model="showServerMessages"/>
       </v-radio-group>
     </div>
     <div class="log" ref="logElement" style="flex: 1 1 100%">
-      <template v-for="message in visibleMessages">
-        <pre :style="styleOfMessage(message)">{{ formatMessageDate(message.time) }} {{ message.text }}</pre>
-      </template>
+      <pre
+        v-for="message in visibleMessages"
+        :style="styleOfMessage(message)"
+        :key="message.uuid"
+      >{{ formatMessageDate(message.time) }} {{ message.text }}</pre>
     </div>
   </div>
 </template>
 
 <script>
-  import { format } from 'date-fns'
+  import {format} from 'date-fns'
   import Vue from 'vue'
+  import logging from '../logging'
+  const uuidv4 = require('uuid/v4')
 
   export default {
     name: 'log-viewer',
-    props: {
-      messages: {
-        type: Array,
-        required: true,
-        // It might be nice to switch to Typescript to avoid the need for this ad-hoc type checking.
-        // This is actually O(n) in the number of log messages, it's really not a great solution.
-        validator: messageList => messageList.every(message =>
-          message.hasOwnProperty('source') &&
-          message.hasOwnProperty('level') &&
-          message.hasOwnProperty('time') &&
-          message.hasOwnProperty('text')
-        )
-      }
-    },
+    props: {},
     data () {
       return {
         logLevel: 2,
         showClientMessages: true,
-        showServerMessages: true
+        showServerMessages: true,
+        messageLog: []
       }
     },
     computed: {
       visibleMessages: function () {
-        return this.messages.filter(m => {
+        return this.messageLog.filter(m => {
           const isSourceVisible = (m.source === 'client' && this.showClientMessages) ||
             (m.source === 'server' && this.showServerMessages)
           const isLogLevelVisible = m.level >= this.logLevel
@@ -55,6 +47,15 @@
         })
       }
 
+    },
+    created: function () {
+      // Subscribe to logging event bus
+      logging.subscribeLog(message => {
+        this.messageLog.push({
+            ...message,
+            uuid: uuidv4()
+          })
+      })
     },
     mounted: function () {
       this.scrollToBottom()
