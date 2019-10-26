1eb4ad9bffc82a96b4fe55e1cb1a107c55f1d10e
Ann Yanich <ann.yanich@posteo.de>
2019-10-26 03:53:45 +0200

    Revert "Disable LogViewer.  It fixes the performance issue"
    This reverts commit 15562f8a

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-java -DPROPERTY_FILE=./ADAM.properties -jar server/target/web-frontend-1.0-SNAPSHOT-jar-with-dependencies.jar 2>&1 | tee adamweb-log-`date +%Y-%m-%d-%Hh%Mm%Ss`.txt &
+java -DPROPERTY_FILE=./ADAM.properties.dev -jar server/target/web-frontend-1.0-SNAPSHOT-jar-with-dependencies.jar 2>&1 | tee adamweb-log-`date +%Y-%m-%d-%Hh%Mm%Ss`.txt &
 
 # Save the pid of the server 
 # echo $! > ../adamweb.pid  # Doesnt work within our pipeline, returns the PID of 'tee'
