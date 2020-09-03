b6f79f63968e7946494085736d3a2cbcd1372fe6
Ann Yanich <ann.yanich@posteo.de>
2020-09-03 14:42:09 +0200

    Revise tab labels for synthesis, e.g. "2-Player Game (C)"

--- client/src/jobType.js
+++ client/src/jobType.js
@@ -3,13 +3,13 @@ export {formatJobType, modelCheckingResultColor}
 function formatJobType (jobType) {
   switch (jobType) {
     case 'GRAPH_GAME_BDD':
-      return 'Graph Game BDD'
+      return '2-Player Game'
     case 'EXISTS_WINNING_STRATEGY':
       return 'Exists winning strategy?'
     case 'WINNING_STRATEGY':
       return 'Winning Strategy'
     case 'GRAPH_STRATEGY_BDD':
-      return 'Graph Strategy BDD'
+      return '2-Player Strategy'
     case 'MODEL_CHECKING_RESULT':
       return 'Model Checking Result'
     case 'MODEL_CHECKING_NET':
