27ad783ade37b51e4249dd13af473e729492443e
mgieseking <gieseking@informatik.uni-oldenburg.de>
2020-09-07 18:03:47 +0200

    Updated the backend to the current version of the submodules.

---
+++ backend/integrate_backend.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# @author Manuel Gieseking
+
+# read the current version number out of the pom.xml
+ver=$(grep -zoP "<artifactId>adam-core</artifactId>[[:space:]]*<version>\K[0-9]+\.[0-9]+(?=</version>)" ../server/pom.xml)
+echo "> Found version ${ver}."
+echo "> Create the adam_core.jar" 
+make deploy_backend 
+echo "> Integrate version ${ver} of adam_core.jar"
+mvn org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file -Dfile=webinterface-backend/adam_core.jar -DgroupId=uniolunisaar.adam -DartifactId=adam-core -Dversion=${ver} -Dpackaging=jar -DlocalRepositoryPath=../server/adam-core
+echo "> Clean up"
+make clean_backend
