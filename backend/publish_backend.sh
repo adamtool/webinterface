87c373df4f2094dbee98ae32cc5ca860ea2d2c31
mgieseking <gieseking@informatik.uni-oldenburg.de>
2020-09-11 00:55:03 +0200

    Updated to the new version (automatic script failed)

--- backend/publish_backend.sh
+++ backend/publish_backend.sh
@@ -3,7 +3,7 @@
 if [[ -z ${ver} ]]; then
 	echo 'You have to give a version number with ver="<version>".'
 else
-    echo "> Create the adam_core.jar" 
+    echo -e "> Create the adam_core.jar" 
     make deploy_backend 
     echo "> Publish the version ${ver} of adam_core.jar"
     mvn org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file -Dfile=webinterface-backend/adam_core.jar -DgroupId=uniolunisaar.adam -DartifactId=adam-core -Dversion=${ver} -Dpackaging=jar -DlocalRepositoryPath=../server/adam-core
