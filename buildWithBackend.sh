ce43cb1e666cffbb99b00bf9ebbb20d9a457631a
mgieseking <gieseking@informatik.uni-oldenburg.de>
2020-09-13 17:12:28 +0200

    Improved the output of the buildWithBackend script.

--- buildWithBackend.sh
+++ buildWithBackend.sh
@@ -9,8 +9,10 @@ ver=$(grep -zoP "<artifactId>adam-core</artifactId>[[:space:]]*<version>\K[0-9]+
 folder=${pref}${ver}
 
 if [ ! -d "${folder}" ]; then
-    printError "> The backend is not compiled for version: ${ver}."
+    printColored "> The backend is not compiled for version: ${ver}." $yellow
     cd backend && make integrate_backend && cd ..
+else 
+    printColored "> The backend is already compiled for version: ${ver}." $green
 fi
 printColored "> update the examples."
 cd backend && make update_examples clean=true && cd ..
