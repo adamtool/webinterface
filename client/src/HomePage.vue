6b7d2375f602112bb237b67a06580f454c615790
Ann Yanich <ann.yanich@posteo.de>
2020-10-14 16:45:28 +0200

    Revise homepage with info about the two modes

--- client/src/HomePage.vue
+++ client/src/HomePage.vue
@@ -1,13 +1,146 @@
 <template>
-  <div>
-    <h1>AdamWEB</h1>
-    <div>
-      <router-link to="/modelChecking">AdamWEB for Model Checking</router-link>
-    </div>
-    <div>
-      <router-link to="/distributedSynthesis">AdamWEB for Distributed Synthesis</router-link>
-    </div>
-  </div>
+  <v-app>
+    <v-container>
+      <v-row
+        style="justify-content: center;"
+      >
+        <v-col
+          cols="12"
+          lg="6"
+          xl="5"
+        >
+          <v-card>
+            <v-card-title
+              primary-title
+              style="justify-content: space-between; word-break: unset;">
+              <router-link to="/modelChecking">
+                AdamMC: A Flow-LTL Model Checker for the Analysis of Distributed Asynchronous
+                Models
+              </router-link>
+            </v-card-title>
+            <v-card-text>
+              <p><strong>AdamMC</strong> is a model checker for asynchronous distributed systems
+                modeled with Petri nets with transits and specifications given in Flow-LTL.</p>
+              <p><strong>Petri nets with transits</strong> extend standard Petri nets with a
+                transit relation to model the local data flow in asynchronous distributed
+                systems in addition to the global control flow.</p>
+              <p><strong>Flow-LTL</strong> is a specification language for Petri nets with
+                transits and allows to specify linear properties on both the global and
+                the local view of the system. Especially, it is possible to globally select with
+                LTL desired runs of the system (e.g., only fair and maximal runs) and check the
+                local data flow of only those selected runs against LTL.</p>
+              <p>Internally the problem is reduced to a verification problem of circuits which
+                is checked by <a href="https://people.eecs.berkeley.edu/~alanmi/abc/"
+                                 rel="nofollow">ABC</a> with its large toolbox of hardware
+                verification algorithms. <strong>Software defined networks</strong> are due to
+                the separation of data and control plane a natural application domain for Petri
+                nets with transits and Flow-LTL.</p>
+              <p>
+              <h2>Features:</h2>
+              <ul>
+                <li>Modeling, visualization, and simulation of Petri nets with transits</li>
+                <li>Model checking Petri nets with transits against Flow-LTL</li>
+                <li>Model checking 1-bounded Petri nets against LTL with places and transitions
+                  as atomic propositions
+                </li>
+                <li>Visualization and simulation of counter examples</li>
+              </ul>
+              </p>
+              <p>
+              <h2>Documentation:</h2></p>
+              <p>
+              <h2>Related Publications: </h2>
+              <ul>
+                <li><em>Bernd Finkbeiner, Manuel Gieseking, Jesko Hecking-Harbusch,
+                  Ernst-Rüdiger
+                  Olderog:</em>
+                  <a href="https://doi.org/10.1007/978-3-030-53291-8_5" rel="nofollow">AdamMC: A
+                    Model
+                    Checker for Petri Nets with Transits against Flow-LTL</a>. CAV (2) 2020:
+                  64-76 (<a
+                    href="https://arxiv.org/abs/2005.07130" rel="nofollow">full version</a>)
+                </li>
+                <li><em>Bernd Finkbeiner, Manuel Gieseking, Jesko Hecking-Harbusch,
+                  Ernst-Rüdiger
+                  Olderog:</em>
+                  <a href="https://doi.org/10.1007/978-3-030-31784-3_30" rel="nofollow">Model
+                    Checking
+                    Data Flows in Concurrent Network Updates</a>. ATVA 2019: 515-533 (<a
+                    href="http://arxiv.org/abs/1907.11061" rel="nofollow">full version</a>)
+                </li>
+              </ul>
+              </p>
+            </v-card-text>
+          </v-card>
+
+        </v-col>
+        <v-col
+          cols="12"
+          lg="6"
+          xl="5"
+        >
+          <v-card>
+            <v-card-title
+              primary-title
+              style="justify-content: space-between; word-break: unset;">
+              <router-link to="/distributedSynthesis"> AdamSYNT: Causality-Based Synthesis of
+                Distributed Asynchronous Systems
+              </router-link>
+            </v-card-title>
+            <v-card-text>
+              <p><strong>AdamSYNT</strong> is a synthesizer for asynchronous distributed systems
+                modeled with
+                Petri games.</p>
+              <p><strong>Petri games</strong> extend Petri nets with a game semantics. The
+                tokens of the net are the players in the game. Players remember their own causal
+                past and do not know nothing about the others as long as they do not communicate
+                (participating on a joint transition). During a communication all information is
+                passed to all participating players.
+              </p>
+              <p>For <strong>local safety winning conditions</strong>, the synthesis problem for
+                Petri games with a bounded number of controllable components and one
+                uncontrollable components is solved, i.e., a local controller for each component
+                is created iff a solution exists.</p>
+              <p>Internally, the problem is reduced to a two-player game over a finite graph
+                with complete information and <strong>BDD</strong> solving algorithms are used
+                to obtain a strategy.</p>
+              <h2>Features:</h2>
+              <ul>
+                <li>Modeling, visualization, and simulation of Petri games</li>
+                <li>Synthesis of Petri games with one environment and a bounded number of system
+                  players with a local safety objective
+                </li>
+                <li>Exploring the corresponding two-player game for finding modeling bugs</li>
+                <li>Visualization and simulation of the strategies</li>
+              </ul>
+              <p>
+              <h2>Documentation:</h2></p>
+              <p>
+              <h2>Releated Publications:</h2>
+              <ul>
+                <li><em>Bernd Finkbeiner, Manuel Gieseking, Ernst-Rüdiger Olderog:</em><br>
+                  <a href="https://doi.org/10.1007/978-3-319-21690-4_25" rel="nofollow">Adam:
+                    Causality-Based Synthesis of Distributed Systems</a>. CAV (1) 2015: 433-439
+                </li>
+                <li><em>Bernd Finkbeiner, Manuel Gieseking, Jesko Hecking-Harbusch,
+                  Ernst-Rüdiger Olderog:</em><br>
+                  <a href="https://doi.org/10.4204/EPTCS.260.5" rel="nofollow">Symbolic vs.
+                    Bounded Synthesis for Petri Games</a>. SYNT@CAV 2017: 23-43
+                </li>
+                <li><em>Bernd Finkbeiner, Ernst-Rüdiger Olderog:</em><br>
+                  <a href="https://doi.org/10.1016/j.ic.2016.07.006" rel="nofollow">Petri games:
+                    Synthesis of distributed systems with causal memory</a>. Inf. Comput. 253:
+                  181-203 (2017)
+                </li>
+              </ul>
+              </p>
+            </v-card-text>
+
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
